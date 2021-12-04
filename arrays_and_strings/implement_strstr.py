9fdb1c4a59caa955595b208f9fdf02cb4d77465e
quashima <ex.rwksm1024@gmail.com>
2021-12-04 23:55:50 +0900

    delete comment

--- arrays_and_strings/implement_strstr.py
+++ arrays_and_strings/implement_strstr.py
@@ -1,21 +1,5 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        # Output Limit Exceeded
-        # if needle == '':
-        #     return 0
-
-        # _str_str = ''
-        # _loop_count = len(haystack) - len(needle) + 1
-        # for _i in range(_loop_count):
-        #     for _n in range(len(needle)):
-        #         _str_str += haystack[_i+_n]
-        #     print(_str_str)
-        #     if _str_str == needle:
-        #         return _i
-        #     else:
-        #         _str_str = ''
-        # return -1
-
         if needle == '':
             return 0
 
