74bf48b969ca3fb1a6a00915db1af988c7d6387c
quashima <ex.rwksm1024@gmail.com>
2021-12-04 23:56:29 +0900

    delete comment

--- arrays_and_strings/two_sum.py
+++ arrays_and_strings/two_sum.py
@@ -1,12 +1,4 @@
 from typing import List
-
-# Time Limit Exceeded
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         for i1, v1 in enumerate(nums):
-#             for i2, v2 in enumerate(nums[1:]):
-#                 if target == (v1+v2) and not i1 == (i2+1):
-#                     return [i1, (i2+1)]
             
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -17,5 +9,6 @@ class Solution:
 
 
 solution = Solution()
-result = solution.twoSum([2,7,11,15], 9)
+# result = solution.twoSum([2,7,11,15], 9)
+result = solution.twoSum([-1,0,1,2,-1,-4], -4)
 print('Ans: ', result)
